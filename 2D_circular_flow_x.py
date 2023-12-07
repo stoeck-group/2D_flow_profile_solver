37f0d0a11c8c5ff207f56629e023212f92d034cd
rhit-zhouy21 <83607751+rhit-zhouy21@users.noreply.github.com>
2023-12-07 15:29:57 -0500

    Update 2D_circular_flow_x.py
    move imports up front

--- 2D_circular_flow_x.py
+++ 2D_circular_flow_x.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python                                                                                                                                                
-"""
-FenicsX !!!
-"""
-"""
-FEniCS tutorial demo program: Deflection of a membrane.
-
-  -Laplace(w) = p  in the unit circle
-            w = 0  on the boundary
-
-The load p is a Gaussian function centered at (0, 0.6).
-"""
+import gmsh
+from dolfinx.io import gmshio
+from mpi4py import MPI
+from dolfinx import fem
+import ufl
+from petsc4py.PETSc import ScalarType
+import numpy as np
+import dolfinx
+from dolfinx.fem.assemble import assemble_matrix,assemble_scalar
+from dolfinx.fem import FunctionSpace, Function, Constant,form
+from dolfinx.plot import create_vtk_mesh
+import pyvista
 
 
 # Create mesh
-import gmsh
 gmsh.initialize()
 membrane = gmsh.model.occ.addDisk(0, 0, 0, 1, 1)
 gmsh.model.occ.synchronize()
@@ -25,31 +25,20 @@ gmsh.model.mesh.generate(gdim)
 gmsh.write('circular_flow_x.msh')
 
 # Interfacing with GMSH in DOLFINx
-from dolfinx.io import gmshio
-from mpi4py import MPI
-
 gmsh_model_rank = 0
 mesh_comm = MPI.COMM_WORLD
 domain, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
 
-from dolfinx import fem
 V = fem.FunctionSpace(domain, ("CG", 1))
 
 # Defining a spatially varying load
-import ufl
-from petsc4py.PETSc import ScalarType
 x = ufl.SpatialCoordinate(domain)
-
-import dolfinx
-
 miu = 10
 dpdz = 10
 p = 1/miu * dpdz
 
 
-
 # Creating a Dirichlet b.c. using geometrical conditions
-import numpy as np
 def on_boundary(x):
     return np.isclose(np.sqrt(x[0]**2 + x[1]**2), 1)
 boundary_dofs = fem.locate_dofs_geometrical(V, on_boundary)
@@ -58,7 +47,6 @@ def boundary(x, on_boundary):
     return on_boundary
 
 bc = fem.dirichletbc(ScalarType(0), boundary_dofs, V)
-
 
 # Define variational problem
 u = ufl.TrialFunction(V)
@@ -74,26 +62,17 @@ uh = problem.solve()
 #pressure = fem.Function(Q)
 #pressure.interpolate(expr)
 
-
-from dolfinx.fem.assemble import assemble_matrix,assemble_scalar
-from dolfinx.fem import FunctionSpace, Function, Constant,form
-
 print('area is')
 one = Constant(domain, ScalarType(1))
 f = form(one*ufl.dx)
 area = assemble_scalar(f)
 print(area)
 
-
 print('mean velocity is')
 f2 = form(uh*ufl.dx)
 mv = assemble_scalar(f2)/area
 print(mv)
 
-
-
-from dolfinx.plot import create_vtk_mesh
-import pyvista
 topology, cell_types, x = create_vtk_mesh(domain, 2)
 grid = pyvista.UnstructuredGrid(topology, cell_types, x)
 
